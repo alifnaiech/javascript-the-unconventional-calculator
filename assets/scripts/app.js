4d1e47d117af5029fe3542098ceb7d0ba05c382d
Ali Fnaiech <alifnaiech@gmail.com>
2021-01-05 09:54:26 +0100

    operation logs

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,65 @@
 let currentResult = 0;
-currentResult = currentResult + 10;
+let logEntries = [];
 
-outputResult(currentResult, '');
+function getUserInput(){
+  return parseInt(userInput.value);
+}
+
+function createAndWriteOuput(operator, resultBeforeCalc, calcNumber){
+  calcDescription = `${calcNumber} ${operator} ${resultBeforeCalc}`;
+  outputResult(currentResult, calcDescription)
+}
+
+function writeToLog(operationIdentifier, preResult, number, result){
+    const logEntry = {
+      operation: operationIdentifier,
+      prevNumer: preResult,
+      nextNumBer: number,
+      result: result
+    };
+    logEntries.push(logEntry);
+    console.log(logEntries);
+};
+
+function addition() {
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult += enteredNumber;
+  createAndWriteOuput('+', initialResult, enteredNumber); 
+  writeToLog('ADD', initialResult, enteredNumber, currentResult);
+}
+
+
+function substraction() {
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult -= enteredNumber;
+  createAndWriteOuput('-', initialResult, enteredNumber);
+  writeToLog('SUB', initialResult, enteredNumber, currentResult);
+}
+
+function multiply() {
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult *= enteredNumber;
+  createAndWriteOuput('*', initialResult, enteredNumber);
+  writeToLog('MUL', initialResult, enteredNumber, currentResult);
+}
+
+function divide() {
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult /= enteredNumber;
+  createAndWriteOuput('/', initialResult, enteredNumber);
+  writeToLog('DIV', initialResult, enteredNumber, currentResult);
+}
+
+addBtn.addEventListener('click', addition);
+subtractBtn.addEventListener('click', substraction);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
+
+
+
+
 
